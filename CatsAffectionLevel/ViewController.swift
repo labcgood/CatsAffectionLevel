e5623717a9bd0fb91050038daa34727f6e8087c3
Rabbit <labcgood44@gmail.com>
2024-01-10 15:35:30 +0800

    Initial Commit

---
+++ CatsAffectionLevel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatsAffectionLevel
+//
+//  Created by Labe on 2024/1/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
